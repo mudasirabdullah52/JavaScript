ac0c1f7e1c67f450ceba79287d897106d4cc0549
mudasirabdullah52 <mudasir.ahmad.mca.2019@miet.ac.in>
2023-10-09 22:56:10 +0530

    how to traver in Dom

--- dom.js
+++ dom.js
@@ -82,16 +82,120 @@
 // secitem.style.color = 'green';
 
 
-// SLECTTOR // GET ELEMENT BY Queary Selector all;
 
-var titles = document.querySelectorAll('.title');
-// console.log(titles)
-titles[0].textContent = 'this list looks good'
-// using the same property on alternates elements
-var odd = document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
+//                                                                  SLECTTOR // GET ELEMENT BY Queary Selector all;
 
-for (var i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor = '#f4f4f4';
-    even[i].style.backgroundColor = '#ccc';
-}
+// var titles = document.querySelectorAll('.title');
+// // console.log(titles)
+// titles[0].textContent = 'this list looks good'
+// // using the same property on alternates elements
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
+
+// for (var i = 0; i < odd.length; i++) {
+//     odd[i].style.backgroundColor = '#f4f4f4';
+//     even[i].style.backgroundColor = '#ccc';
+// }
+
+
+
+//                                          Traversing DOM
+
+//                                          parent Node
+// var itemList = document.querySelector('#items');
+// // access the parent of itemList
+// itemList.parentNode.style.backgroundColor = "#f4f4f4";
+// // acessing the parent of parent
+// var pop = itemList.parentNode.parentNode;
+// pop.style.backgroundColor = "cyan"
+
+//                                        PARENT ELEMENT
+
+// var itemList = document.querySelector('#items');
+// // access the parent of itemList
+// itemList.parentElement.style.backgroundColor = "#f4f4f4";
+// // acessing the parent of parent
+// var pop = itemList.parentElement.parentElement;
+// pop.style.backgroundColor = "cyan"
+
+//                                         chlid nodes    // return nodes list
+
+// var itemList = document.querySelector('#items');
+// console.log(itemList.childNodes)
+
+//                                         CHILDEREN NODES USE ALWAYS// return html collection
+
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow'
+
+//                                      FIRST CHILD
+
+// console.log(itemList.firstChild)
+
+//                                      FIRST ELEMENT CHILD
+
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Mudasir is a good boy';
+
+
+//                                      LAST CHILD
+// console.log(itemList.lastChild)
+
+
+//                                      LAST eLEMENT CHILD
+
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Mudasir is working very hard ';
+
+
+//                                      NEXT SIBLING
+
+
+// var itemList = document.querySelector('#items');
+// console.log(itemList.nextSibling)
+
+//                                 NEXT ELEMENT sIBLING
+// console.log(itemList.nextElementSibling);
+
+//                                  previous sibling
+
+// console.log(itemList.previousSibling)
+
+//                                  PREVIOUS ELEMENT SIBLING
+
+// console.log(itemList.previousElementSibling)
+// itemList.previousElementSibling.style.color = "red";
+
+
+//                                                Create element
+
+var newDiv = document.createElement("div");
+// add class
+newDiv.className = 'hello'
+// add id
+newDiv.id = "hello-id"
+//   add attribute
+newDiv.setAttribute('titel', "hello Div")
+// create a text node
+var newDivText = document.createTextNode("hello world");
+// add text to div
+newDiv.appendChild(newDivText)
+
+// insert this div into to the DOM
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv)
+
+newDiv.style.fontSize = '30px';
+newDiv.style.fontFamily = 'bold';
+container.insertBefore(newDiv, h1);
+
+
+
+
+
+
+
