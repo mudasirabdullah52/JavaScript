eca1e2f9b4e26911a06b051a95a074de6d312c16
mudasirabdullah52 <mudasir.ahmad.mca.2019@miet.ac.in>
2023-10-10 11:00:10 +0530

    implementatoin of dob

---
+++ dom2.js
@@ -0,0 +1,75 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// form submit event
+form.addEventListener('submit', addItem);
+// Delete item from the list
+itemList.addEventListener('click', removeItem);
+// filter the elements
+filter.addEventListener('keyup', filterItems);
+
+// Add item 
+function addItem(e) {
+    e.preventDefault();
+
+    // Get input value 
+    var newItem = document.getElementById('item').value;
+
+    // Create new li element
+
+    var li = document.createElement('li');
+
+    // add class to the list
+    li.className = 'list-group-item'
+
+    // apppend the text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    // Create the delete button element
+
+    var deleteBtn = document.createElement('button');
+    // add class to deleteBtn 
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    // add text node to the button 
+    deleteBtn.appendChild(document.createTextNode('X'))
+    // append this delete button to li 
+    li.appendChild(deleteBtn)
+
+    // append li element to itemList
+
+    itemList.appendChild(li);
+
+
+    console.log(li)
+}
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you sure to Delete')) {
+
+            // get the parent of the target element  
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+function filterItems(e) {
+    // convert the text into lower case;
+    var text = e.target.value.toLowerCase();
+    // get list
+    var items = itemList.getElementsByTagName('li');
+    // make the array go lists
+    Array.from(items).forEach(function (item) {
+        // get the text of list element 
+        var itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+
+    })
+
+
+}
