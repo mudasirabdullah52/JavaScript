dd058db5183378fa5e9d7e7c2825bd485980189c
mudasirabdullah52 <mudasir.ahmad.mca.2019@miet.ac.in>
2023-10-11 15:38:44 +0530

    added edit funcationality

--- Add-data-local-strorage/main.js
+++ Add-data-local-strorage/main.js
@@ -1,54 +1,68 @@
-var form = document.getElementById('inner-form');
+// var form = document.getElementById('inner-form');
 
 
-var msg = document.getElementById('msg');
-var users = document.getElementById('users');
+// var msg = document.getElementById('msg');
+// var users = document.getElementById('users');
 
-users.addEventListener('click', removeItem);
+// users.addEventListener('click', removeItem);
 
-form.addEventListener('submit', addItem);
+// form.addEventListener('submit', addItem);
 
-function addItem(e) {
+// function addItem(e) {
+//     e.preventDefault();
+//     var name = document.getElementById('name').value;
+//     var email = document.getElementById('email').value;
+//     var phone = document.getElementById('phone').value;
+
+//     // Creating the li for user info
+//     var li = document.createElement('li');
+//     li.appendChild(document.createTextNode(name + ' ' + email + ' ' + phone));
+
+//     // Add button
+//     var deleteBtn = document.createElement('button');
+//     deleteBtn.className = 'delete'
+//     deleteBtn.appendChild(document.createTextNode('delete'));
+//     li.appendChild(deleteBtn);
+
+//     // getting the list and add the li to
+//     var users = document.getElementById('users');
+//     users.appendChild(li);
+
+//     // Add Data into the localStorage
+//     var myobj = {
+//         myname: name,
+//         myemai: email,
+//         nyphone: phone
+//     };
+
+//     // converting the object into string
+//     var myobj_serkauzed = JSON.stringify(myobj);
+//     localStorage.setItem(name, myobj_serkauzed);
+//     console.log(localStorage);
+
+
+// }
+// function removeItem(e) {
+//     e.preventDefault();
+//     if (e.target.classList.contains('delete')) {
+//         if (confirm('Are you sure to Delete')) {
+//             // get the parent of the target element
+//             var li = e.target.parentElement;
+
+//             users.removeChild(li);
+//         }
+//     }
+// }
+
+
+
+
+
+// NOW TRY TO DO THE ABOVE WITH DIFFERNT WAY
+
+
+
+function saveOnLocal(e) {
     e.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-
-    // Creating the li for user info
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(name + ' ' + email + ' ' + phone));
-
-    // Add button 
-    var deleteBtn = document.createElement('button');
-    deleteBtn.className = 'delete'
-    deleteBtn.appendChild(document.createTextNode('delete'));
-    li.appendChild(deleteBtn);
-
-    // getting the list and add the li to
-    var users = document.getElementById('users');
-    users.appendChild(li);
-
-    // Add Data into the localStorage
-    var myobj = {
-        myname: name,
-        myemai: email,
-        nyphone: phone
-    };
-
-    // converting the object into string 
-    var myobj_serkauzed = JSON.stringify(myobj);
-    localStorage.setItem(name, myobj_serkauzed);
-    console.log(localStorage);
-
-
-}
-function removeItem(e) {
-    e.preventDefault();
-    if (e.target.classList.contains('delete')) {
-        if (confirm('Are you sure to Delete')) {
-            // get the parent of the target element  
-            var li = e.target.parentElement;
-            users.removeChild(li);
-        }
-    }
+    console.log(1)
 }
