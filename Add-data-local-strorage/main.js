fa9e0862a84eb536ec0c574005aa37cd519fffb9
mudasirabdullah52 <mudasir.ahmad.mca.2019@miet.ac.in>
2023-10-11 11:01:10 +0530

    stored the user in local storage and showed on the display as well

--- Add-data-local-strorage/main.js
+++ Add-data-local-strorage/main.js
@@ -10,22 +10,25 @@ function addItem(e) {
     e.preventDefault();
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
 
+    // Creating the li for user info
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(name + ' ' + email + ' ' + phone));
 
-    // var li = document.createElement('li');
-    // li.appendChild(document.createTextNode(name));
+    // getting the list and add the li to
+    var users = document.getElementById('users');
+    users.appendChild(li);
 
-    // var users = document.getElementById('users');
-    // users.appendChild(li);
     // Add Data into the localStorage
     var myobj = {
         myname: name,
-        myemai: email
-
+        myemai: email,
+        nyphone: phone
     };
 
+    // converting the object into string 
     var myobj_serkauzed = JSON.stringify(myobj);
-    // console.log(myobj_serkauzed);
     localStorage.setItem(name, myobj_serkauzed);
     console.log(localStorage);
 
